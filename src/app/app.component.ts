640b6314721bc456d71f41b2d83f5fb2ea44036a
kolomu <kevin-olomu@web.de>
2018-09-22 08:06:28 +0200

    implement ngx-masonry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+    `
+       .masonry-item { width: 200px; }
+     `
+  ]
 })
 export class AppComponent {
-  title = 'NGMasonryExample';
+  masonryItems = [
+    { title: 'item 1' },
+    { title: 'item 2' },
+    { title: 'item 3' },
+    { title: 'item 4' },
+    { title: 'item 5' },
+    { title: 'item 6' }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxMasonryModule } from 'ngx-masonry';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMasonryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
